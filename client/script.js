95bc579ff5fde1206d8266d0f92266e513830bcd
mali295 <mali295@uwo.ca>
2022-12-28 16:20:26 -0500

    Create front-end for user

---
+++ client/script.js
@@ -0,0 +1,32 @@
+import bot from './assets/bot.svg'
+import user from './assets/user.svg'
+
+const form = document.querySelector('form')
+const chatContainer = document.querySelector('#chat_container')
+
+let loadInterval;
+
+function loader(element) {
+  element.textContent = '';
+
+  loadInterval = setInterval(() => {
+    element.textContent += '';
+
+    if (element.textContent === '....') {
+      element.textContent = '';
+    }
+  }, 300)
+}
+
+function typeText(element, text) {
+  let index = 0
+
+  let interval = setInterval(() => {
+    if (index < text.length) {
+      element.innerHTML = text.charAt(index)
+      index++
+    } else {
+      clearInterval(interval)
+    }
+  }, 20)
+}
