7f4b21e0b460442f5e9dae0c5acfdf76513b36a3
mali295 <mali295@uwo.ca>
2022-12-28 17:22:53 -0500

    Fetch data from the server

--- client/script.js
+++ client/script.js
@@ -44,7 +44,7 @@ function chatStripe(isAi, value, uniqueId) {
     `
       <div class="wrapper ${isAi && 'ai'}">
         <div class="chat">
-          <div className="profile">
+          <div class="profile">
             <img src="${isAi ? bot: user}" alt="${isAi ? 'bot': 'user'}" />
           </div>
           <div class="message" id=${uniqueId}>${value}</div>
@@ -72,6 +72,35 @@ const handleSubmit = async (e) => {
   const messageDiv = document.getElementById(uniqueId)
 
   loader(messageDiv);
+
+  // fetch data from server
+
+  const response = await fetch('http://localhost:5000', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      prompt: data.get("prompt")
+    })
+  })
+
+  clearInterval(loadInterval);
+  messageDiv.innerHTML = ''
+
+  if(response.ok) {
+    const data = await response.json()
+    const parsedData = data.bot.trim()
+
+    typeText(messageDiv, parsedData) 
+  } else {
+    const err = await response.text()
+
+    messageDiv.innerHTML = "Something went wrong"
+
+    alert(err)
+  }
+
 }
 
 form.addEventListener('submit', handleSubmit)
