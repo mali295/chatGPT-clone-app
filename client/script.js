2a0d6f62086b01c81c9bd4d5caa5cc685e98f655
mali295 <mali295@uwo.ca>
2022-12-28 16:34:37 -0500

    Add submit button functionality

--- client/script.js
+++ client/script.js
@@ -53,3 +53,31 @@ function chatStripe(isAi, value, uniqueId) {
     `
   )
 }
+
+const handleSubmit = async (e) => {
+  e.preventDefault();
+
+  const data = new FormData(form);
+
+  // user's chat's stripe
+  chatContainer.innerHTML += chatStripe(false, data.get("prompt"));
+
+  form.reset()
+
+  const uniqueId = generateUniqueId()
+  chatContainer.innerHTML += chatStripe(true, " ", uniqueId)
+
+  chatContainer.scrollTop = chatContainer.scrollHeight;
+
+  const messageDiv = document.getElementById(uniqueId)
+
+  loader(messageDiv);
+}
+
+form.addEventListener('submit', handleSubmit)
+form.addEventListener('keyup', (e) => {
+  if(e.keyCode === 13) {
+    handleSubmit(e)
+  }
+})
+
